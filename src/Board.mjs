2bffe770c5d3f5616f7df6cf31eba46cd23331bc
Teemu Kurki <teemu.kurki@op.fi>
2024-05-22 09:39:37 +0300

    implement board.drop method and fix it starts from the top middle test

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,15 +1,20 @@
 export class Board {
   width;
   height;
-
+  initialRow;
   constructor(width, height) {
-    this.width = width;
-    this.height = height;
+      this.width = width;
+      this.height = height;
+      this.initialRow = "...\n"
   }
 
+  drop(icon) {
+    this.initialRow = `.${icon}.\n`
+ }
+
   toString() {
-    let board = ""
-    for (let y = 0; y < this.height; y++) {
+    let board = this.initialRow
+    for (let y = 1; y < this.height; y++) {
         for (let x = 0; x < this.width; x++) {
             board += "."
         }
--- test/FallingBlocks.test.mjs
+++ test/FallingBlocks.test.mjs
@@ -16,7 +16,7 @@ describe("Falling blocks", () => {
     );
   });
 
-  /*
+  
   describe("When a block is dropped", () => {
     beforeEach(() => {
       board.drop("X");
@@ -29,7 +29,7 @@ describe("Falling blocks", () => {
          ...`
       );
     });
-
+/*
     test.skip("it moves down one row per tick", () => {
       board.tick();
 
@@ -46,8 +46,8 @@ describe("Falling blocks", () => {
       const after = board.toString();
       expect(after).to.equal(before);
     });
+    */
   });
-  */
 
   /*
   describe("When a block reaches the bottom", () => {
