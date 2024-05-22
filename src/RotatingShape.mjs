283d15986ab6a040e03006548cd2377485b0326d
Teemu Kurki <teemu.kurki@op.fi>
2024-05-22 16:43:27 +0300

    create rotateRIght implementation

---
+++ src/RotatingShape.mjs
@@ -0,0 +1,39 @@
+
+const createEmptyShape = (shape) => {
+    const newArray = new Array(shape.length)
+    for (let y = 0; y < shape.length; y++) {
+        const row = shape[y]
+        newArray[y] = new Array(row.length)
+    }
+    return newArray;
+}
+
+class Shape {
+    shape;
+    constructor(shape){
+        this.shape = shape
+    }
+    rotateRight() {
+        const rotatedShape = createEmptyShape(this.shape);
+        for (let y = 0; y < this.shape.length; y++) {
+            const row = this.shape[y]
+            for (let x = 0; x < row.length; x++) {
+                rotatedShape[x][row.length - 1 - y] = this.shape[y][x]
+            }
+            
+        }
+        return new Shape(rotatedShape.map(row => row.join("")))
+    }
+    toString() {
+        return this.shape.flat().join("\n") + "\n"
+    }
+}
+
+export const RotatingShape = {
+    fromString(str){
+        const shapeStr = str.replaceAll(" ", "").trim()
+        const shape = shapeStr.split("\n");
+        return new Shape(shape)
+    }
+    
+}
