492709d684448ed2633295d5d353cd00e15dad01
Teemu Kurki <teemu.kurki@op.fi>
2024-05-22 12:45:25 +0300

    stop block on top of other blocks

--- src/Board.mjs
+++ src/Board.mjs
@@ -29,15 +29,19 @@ export class Board {
     
 }
 
+ atBottom(currentBlock){
+    return this.blocks.some(block => currentBlock[0][0] === block[0][0] && currentBlock[0][1] === block[0][1] - 1) || currentBlock[0][1] === this.height -1
+ }
+
  tick(){
-    if(this.block[0][1] > -1 && this.block[0][1] !== this.height -1) {
+    if(this.block[0][1] > -1 && !this.atBottom(this.block)) {
         this.block[0][1] += 1
     }
     // Allow falling one tick after touching bottom
     if(this.blockAtBottom){
         this.falling = false
     }   
-    if(this.block[0][1] === this.height -1){
+    if(this.atBottom(this.block)){
         this.blockAtBottom = true
     } 
  }
