884cc187c7c4091a82f81159414b66cb8fa5761d
Teemu Kurki <teemu.kurki@op.fi>
2024-05-28 09:13:15 +0300

    create Tetromino.mjs

---
+++ src/Tetromino.mjs
@@ -0,0 +1,14 @@
+import { RotatingShape } from "./RotatingShape.mjs";
+
+export const Tetromino = {
+
+
+    T_SHAPE:RotatingShape.fromString(
+        `.T.
+         TTT
+         ...`
+    )
+    
+
+
+}
